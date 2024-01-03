e8fcd7180234881f38e689ae5e7080134b45bade
John Vouvakis Manousakis <ioannis_vm@berkeley.edu>
2024-01-02 19:16:51 -0800

    Add two more Weibul models & refactor
    - Add two simpler Weibul models to confirm that MLE works
    - Rename `params` to `parameters`. https://peps.python.org/pep-0020/
    - Improve plotting

--- src/fit_models.py
+++ src/fit_models.py
@@ -4,10 +4,12 @@ Fit all models to the data and store the parameters.
 
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.lines import Line2D
 import seaborn as sns
 from src.models import Model_0_P58
 from src.models import Model_1_Weibull
-
+from src.models import Model_1v0_Weibull
+from src.models import Model_1v1_Weibull
 
 def main():
     """
@@ -25,8 +27,8 @@ def main():
     df = only_drifts(remove_collapse(load_dataset()[0]))
     case_df = df[the_case].dropna()
 
-    # limit to given hazard level
-    case_df = case_df.xs('2', level=0).dropna()
+    # # limit to given hazard level
+    # case_df = case_df.xs('2', level=0).dropna()
 
     rid_vals = case_df.dropna()["RID"].to_numpy().reshape(-1)
     pid_vals = case_df.dropna()["PID"].to_numpy().reshape(-1)
@@ -48,6 +50,7 @@ def main():
 
     model = Model_1_Weibull()
     model.add_data(pid_vals, rid_vals)
+    model.censoring_limit = 0.002
     model.fit(method='mle')
 
     fig, ax = plt.subplots()
@@ -83,38 +86,178 @@ def main():
     plt.show()
 
 
-    # Weibull, MLE on generated samples
+    ###############################
+    # Weibull with two parameters #
+    ###############################
 
-    model = Model_1_Weibull()
-    model.params = np.array((0.0045 , 0.3, 2.50))
+    model = Model_1v0_Weibull()
+    model.parameters = (0.03, 1.30)
 
-    pid_samples = np.random.choice(pid_vals, size=1000, replace=True)
-    # pid_samples = np.random.uniform(0.0, 0.06, size=1000)
-    rid_samples = model.generate_rid_samples(pid_samples)
+    # Confirms:
+    # - that the pdf looks the same when plotted in Mathematica
+    rid = np.linspace(0.00, 0.08, 10000)
+    pid = np.linspace(0.00, 0.08, 10000)
+    # pdf_values = model.evaluate_pdf(rid, pid)
+    # fig, ax = plt.subplots()
+    # ax.plot(rid, pdf_values)
+    # plt.show()
 
-    model = Model_1_Weibull()
-    model.add_data(pid_samples, rid_samples)
-    model.fit(method='quantiles')
-    print(model.params)
-    # np.array((0.0045 , 0.3, 2.50))
+    # Confirms:
+    # - that the empirical CDF of the generated samples matches the
+    #   theoretical CDF
+    rid_samples = model.generate_rid_samples(pid)
+    # cdf_val = model.evaluate_cdf(rid_samples, pid)
+    # fig, ax = plt.subplots()
+    # sns.ecdfplot(rid_samples, ax=ax)
+    # ax.scatter(rid_samples, cdf_val, color='C1', s=1)
+    # plt.show()
 
-    model = Model_1_Weibull()
-    model.add_data(pid_samples, rid_samples)
-    model.fit(method='mle')
-    print(model.params)
-    # np.array((0.0045 , 0.3, 2.50))
-
-
+    # Fit another model to get back the parameters
+    other_model = Model_1v0_Weibull()
+    other_model.add_data(pid, rid_samples)
+    other_model.fit(method='mle') # Can change to `quantiles` for experimentation.
+    print(other_model.parameters)
     fig, ax = plt.subplots()
-    model.plot_model(ax)
-    ax.set(xlim=(-0.005, 0.025), ylim=(-0.005, 0.06))
-    ax.fill_between((0.002, 0.015), -1, 1, alpha=0.30, color='black')
+    model.plot_model(ax, rolling=False, training=False, model=True, model_color='C0')
+    other_model.plot_model(ax, rolling=False, training=True, model=True, model_color='C1')
+    ax.legend(
+        [
+         Line2D([0], [0], color='C0', lw=1),
+         Line2D([0], [0], color='C1', lw=1),
+        ],
+        ['Assigned', 'Retrieved']
+    )
     plt.show()
 
-    # plot a slice
-    pid_min = 0.01
-    pid_max = 0.015
+    #########################################
+    # Weibull with two parameters, censored #
+    #########################################
+
+    model = Model_1v0_Weibull()
+    model.parameters = (0.03, 1.30)
+
+    rid = np.linspace(0.00, 0.08, 10000)
+    pid = np.linspace(0.00, 0.08, 10000)
+
+    rid_samples = model.generate_rid_samples(pid)
+
+    other_model = Model_1v0_Weibull()
+    other_model.add_data(pid, rid_samples)
+    other_model.censoring_limit = 0.002 # Modify this. Works up to 0.07! wow.
+    other_model.fit(method='mle')
+    print(other_model.parameters)
     fig, ax = plt.subplots()
-    model.plot_slice(ax, pid_min, pid_max, 0.002)
+    model.plot_model(ax, rolling=False, training=False, model=True, model_color='C0')
+    other_model.plot_model(ax, rolling=False, training=True, model=True, model_color='C1')
+    ax.axvline(x=other_model.censoring_limit, linestyle='dashed', color='black')
+    ax.legend(
+        [
+         Line2D([0], [0], color='C0', lw=1),
+         Line2D([0], [0], color='C1', lw=1),
+        ],
+        ['Assigned', 'Retrieved']
+    )
+    plt.show()
+
+    ##########################################
+    # Weibull with linear lambda function #
+    ##########################################
+
+    model = Model_1v1_Weibull()
+    model.parameters = (0.30, 1.30) # c_lamda_slope, c_kapa
+
+    rid = np.linspace(0.00, 0.08, 1000)
+    pid = np.linspace(0.00, 0.08, 1000)
+
+    rid_samples = model.generate_rid_samples(pid)
     
+    other_model = Model_1v1_Weibull()
+    other_model.add_data(pid, rid_samples)
+    other_model.censoring_limit = 0.002
+    other_model.fit(method='mle')
+    print(other_model.parameters)
+    plt.close()
+    fig, ax = plt.subplots()
+    model.plot_model(ax, rolling=False, training=False, model=True, model_color='C0')
+    other_model.plot_model(ax, rolling=False, training=False, model=True, model_color='C1')
+    ax.axvline(x=other_model.censoring_limit, linestyle='dashed', color='black')
+    ax.legend(
+        [
+         Line2D([0], [0], color='C0', lw=1),
+         Line2D([0], [0], color='C1', lw=1),
+        ],
+        ['Assigned', 'Retrieved']
+    )
     plt.show()
+
+    ##########################################
+    # Weibull with piecewise lambda function #
+    ##########################################
+
+    model = Model_1_Weibull()
+    model.parameters = (0.01, 0.30, 1.30) # c_lamda_slope, c_kapa
+
+    rid = np.linspace(0.00, 0.08, 30)
+    pid = np.linspace(0.00, 0.08, 30)
+
+    rid_samples = model.generate_rid_samples(pid)
+    
+    other_model = Model_1_Weibull()
+    other_model.add_data(pid, rid_samples)
+    other_model.censoring_limit = 0.002
+    other_model.fit(method='mle')
+    print(other_model.parameters)
+    plt.close()
+    fig, ax = plt.subplots()
+    model.plot_model(ax, rolling=False, training=False, model=True, model_color='C0')
+    other_model.plot_model(ax, rolling=False, training=False, model=True, model_color='C1')
+    ax.axvline(x=other_model.censoring_limit, linestyle='dashed', color='black')
+    ax.legend(
+        [
+         Line2D([0], [0], color='C0', lw=1),
+         Line2D([0], [0], color='C1', lw=1),
+        ],
+        ['Assigned', 'Retrieved']
+    )
+    plt.show()
+
+    # Fit multiple times, get distribution for the parameters
+    
+    def get_params():
+        model = Model_1_Weibull()
+        model.parameters = (0.01, 0.30, 1.30) # c_lamda_slope, c_kapa
+
+        rid = np.linspace(0.00, 0.08, 30)
+        pid = np.linspace(0.00, 0.08, 30)
+
+        rid_samples = model.generate_rid_samples(pid)
+
+        other_model = Model_1_Weibull()
+        other_model.add_data(pid, rid_samples)
+        other_model.censoring_limit = 0.002
+        other_model.fit(method='mle')
+        return other_model.parameters
+
+    params = []
+    from tqdm import trange
+    for _ in trange(100):
+        params.append(get_params())
+
+    res = np.concatenate(params)
+    res = res.reshape(-1, 3)
+
+    fig, axs = plt.subplots(3, 1)
+    sns.ecdfplot(res[:, 0], ax=axs[0])
+    axs[0].axvline(x=0.01)
+    axs[0].set(xlim=(0.00, 0.02))
+    sns.ecdfplot(res[:, 1], ax=axs[1])
+    axs[1].axvline(x=0.30)
+    axs[1].set(xlim=(0.00, 0.60))
+    sns.ecdfplot(res[:, 2], ax=axs[2])
+    axs[2].axvline(x=1.30)
+    axs[2].set(xlim=(0.00, 2.60))
+    plt.show()
+
+if __name__ == '__main__':
+
+    main()
