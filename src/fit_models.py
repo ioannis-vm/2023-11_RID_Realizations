d4f660fa6b3377f65a7b22a23ea230326a7cabde
John Vouvakis Manousakis <ioannis_vm@berkeley.edu>
2024-01-04 13:12:22 -0800

    Add code that fits to all results

--- src/fit_models.py
+++ src/fit_models.py
@@ -2,18 +2,71 @@
 Fit all models to the data and store the parameters.
 """
 
+import pandas as pd
 import matplotlib.pyplot as plt
+from itertools import product
+import tqdm
 from src.models import Model_0_P58
 from src.models import Model_1_Weibull
 from src.handle_data import load_dataset
 from src.handle_data import remove_collapse
 from src.handle_data import only_drifts
+from src.util import store_info
+
+
+def get_all_cases():
+
+    cases = []
+    for sys, st, rc, dr in product(
+        ('smrf', 'scbf', 'brbf'), # system
+        ('3', '6', '9'),          # number of stories
+        ('ii', 'iv'),             # risk category
+        ('1', '2')                # direction (X, Y)
+    ):
+        for lv in range(1, int(st)+1):
+            lv = int(lv)
+            cases.append((sys, st, rc, str(lv), dr))
+
+    return cases
+
+
+def obtain_weibul_parameters():
+
+    df = only_drifts(remove_collapse(load_dataset()[0]))
+
+    cases = get_all_cases()
+
+    parameters = []
+    loglikelihood = []
+
+    for the_case in tqdm.tqdm(cases):
+        case_df = df[the_case].dropna()
+        rid_vals = case_df.dropna()["RID"].to_numpy().reshape(-1)
+        pid_vals = case_df.dropna()["PID"].to_numpy().reshape(-1)
+
+        model = Model_1_Weibull()
+        model.add_data(pid_vals, rid_vals)
+        model.censoring_limit = 0.0005
+        model.fit(method='mle')
+        loglikelihood.append(-model.fit_meta.fun)
+        parameters.append(model.parameters)
+
+    res = pd.DataFrame(
+        parameters,
+        index=pd.MultiIndex.from_tuples(
+            cases, names=('system', 'stories', 'rc', 'story', 'direction')
+        ),
+        columns=['c_pid_0', 'c_lamda_slope', 'c_kapa']
+    )
+    res['loglikelihood'] = loglikelihood
+    res.sort_index(inplace=True)
+    res.to_parquet(store_info('results/parameters/weibul_bilinear/parameters.parquet', ['data/edp.parquet']))
 
 
 def main():
 
 
-    the_case = ("smrf", "3", "ii", "1", "1")
+    the_case = ('scbf', '3', 'ii', '3', '1')
     # the_case = ("scbf", "9", "ii", "3", "1")
     # the_case = ("brbf", "9", "ii", "1", "1")
 
@@ -26,24 +79,24 @@ def main():
     rid_vals = case_df.dropna()["RID"].to_numpy().reshape(-1)
     pid_vals = case_df.dropna()["PID"].to_numpy().reshape(-1)
 
-    # FEMA P-58
+    # # FEMA P-58
 
-    model = Model_0_P58()
-    model.add_data(pid_vals, rid_vals)
-    model.fit(0.007, 0.90)      # values hand-picked for ("smrf", "9", "ii", "1", "1")
+    # model = Model_0_P58()
+    # model.add_data(pid_vals, rid_vals)
+    # model.fit(delta_y=0.01, beta=0.60)      # values hand-picked for ("smrf", "9", "ii", "1", "1")
 
-    _, ax = plt.subplots()
-    model.plot_model(ax)
-    ax.set(xlim=(-0.005, 0.08), ylim=(-0.005, 0.08))
-    ax.fill_between((0.002, 0.015), -1, 1, alpha=0.30, color='black')
-    plt.show()
+    # _, ax = plt.subplots()
+    # model.plot_model(ax)
+    # ax.set(xlim=(-0.005, 0.08), ylim=(-0.005, 0.08))
+    # ax.fill_between((0.002, 0.015), -1, 1, alpha=0.30, color='black')
+    # plt.show()
 
 
     # Weibull, MLE
 
     model = Model_1_Weibull()
     model.add_data(pid_vals, rid_vals)
-    model.censoring_limit = 0.002
+    model.censoring_limit = 0.0005
     model.fit(method='mle')
 
     _, ax = plt.subplots()
