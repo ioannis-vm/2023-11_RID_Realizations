ec3bc723243f30050e6355d94ab956e97f4ada47
John Vouvakis Manousakis <ioannis_vm@berkeley.edu>
2024-01-04 13:17:22 -0800

    linting

--- src/fit_models.py
+++ src/fit_models.py
@@ -6,7 +6,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from itertools import product
 import tqdm
-from src.models import Model_0_P58
 from src.models import Model_1_Weibull
 from src.handle_data import load_dataset
 from src.handle_data import remove_collapse
@@ -15,15 +14,14 @@ from src.util import store_info
 
 
 def get_all_cases():
-
     cases = []
     for sys, st, rc, dr in product(
-        ('smrf', 'scbf', 'brbf'), # system
-        ('3', '6', '9'),          # number of stories
-        ('ii', 'iv'),             # risk category
-        ('1', '2')                # direction (X, Y)
+        ('smrf', 'scbf', 'brbf'),  # system
+        ('3', '6', '9'),  # number of stories
+        ('ii', 'iv'),  # risk category
+        ('1', '2'),  # direction (X, Y)
     ):
-        for lv in range(1, int(st)+1):
+        for lv in range(1, int(st) + 1):
             lv = int(lv)
             cases.append((sys, st, rc, str(lv), dr))
 
@@ -31,7 +29,6 @@ def get_all_cases():
 
 
 def obtain_weibull_parameters():
-
     df = only_drifts(remove_collapse(load_dataset()[0]))
 
     cases = get_all_cases()
@@ -56,16 +53,19 @@ def obtain_weibull_parameters():
         index=pd.MultiIndex.from_tuples(
             cases, names=('system', 'stories', 'rc', 'story', 'direction')
         ),
-        columns=['c_pid_0', 'c_lamda_slope', 'c_kapa']
+        columns=['c_pid_0', 'c_lamda_slope', 'c_kapa'],
     )
     res['loglikelihood'] = loglikelihood
     res.sort_index(inplace=True)
-    res.to_parquet(store_info('results/parameters/weibull_bilinear/parameters.parquet', ['data/edp.parquet']))
+    res.to_parquet(
+        store_info(
+            'results/parameters/weibull_bilinear/parameters.parquet',
+            ['data/edp.parquet'],
+        )
+    )
 
 
 def main():
-
-
     the_case = ('scbf', '3', 'ii', '3', '1')
     # the_case = ("scbf", "9", "ii", "3", "1")
     # the_case = ("brbf", "9", "ii", "1", "1")
@@ -80,17 +80,18 @@ def main():
     pid_vals = case_df.dropna()["PID"].to_numpy().reshape(-1)
 
     # # FEMA P-58
-
+    # from src.models import Model_0_P58
     # model = Model_0_P58()
     # model.add_data(pid_vals, rid_vals)
-    # model.fit(delta_y=0.01, beta=0.60)      # values hand-picked for ("smrf", "9", "ii", "1", "1")
+    # model.fit(
+    #     delta_y=0.01, beta=0.60
+    # )  # values hand-picked for ("smrf", "9", "ii", "1", "1")
 
     # _, ax = plt.subplots()
     # model.plot_model(ax)
     # ax.set(xlim=(-0.005, 0.08), ylim=(-0.005, 0.08))
     # ax.fill_between((0.002, 0.015), -1, 1, alpha=0.30, color='black')
     # plt.show()
-
 
     # Weibull, MLE
 
@@ -132,7 +133,5 @@ def main():
     plt.show()
 
 
-
 if __name__ == '__main__':
-
     main()
