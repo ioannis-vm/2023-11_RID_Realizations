1c0cec445afabdd118460f4da16bafe2b6a8b8c5
John Vouvakis Manousakis <ioannis_vm@berkeley.edu>
2024-01-09 05:24:50 -0800

    Updates
    adds the new models

--- src/validation/basic_fit.py
+++ src/validation/basic_fit.py
@@ -5,15 +5,15 @@ Fit all models to the data and store the parameters.
 import matplotlib.pyplot as plt
 from src.models import Model_0_P58
 from src.models import Model_1_Weibull
+from src.models import Model_2_Gamma
+from src.models import Model_3_Beta
 from src.handle_data import load_dataset
 from src.handle_data import remove_collapse
 from src.handle_data import only_drifts
 
 
 def main():
-    the_case = ('scbf', '3', 'ii', '3', '1')
-    # the_case = ("scbf", "9", "ii", "3", "1")
-    # the_case = ("brbf", "9", "ii", "1", "1")
+    the_case = ("smrf", "3", "ii", "3", "1")
 
     df = only_drifts(remove_collapse(load_dataset()[0]))
     case_df = df[the_case].dropna()
@@ -76,6 +76,46 @@ def main():
     model.plot_slice(ax, pid_min, pid_max, 0.002)
     plt.show()
 
+
+    # Gamma, MLE
+
+    model = Model_2_Gamma()
+    model.add_data(pid_vals, rid_vals)
+    model.censoring_limit = 0.0005
+    model.fit(method='mle')
+
+    _, ax = plt.subplots()
+    model.plot_model(ax)
+    ax.set(xlim=(-0.005, 0.025), ylim=(-0.005, 0.06))
+    ax.fill_between((0.002, 0.015), -1, 1, alpha=0.30, color='black')
+    plt.show()
+
+    # plot a slice
+    pid_min = 0.01
+    pid_max = 0.015
+    _, ax = plt.subplots()
+    model.plot_slice(ax, pid_min, pid_max, 0.002)
+    plt.show()
+
+    # Beta, MLE
+
+    model = Model_3_Beta()
+    model.add_data(pid_vals, rid_vals)
+    model.censoring_limit = 0.0005
+    model.fit(method='mle')
+
+    _, ax = plt.subplots()
+    model.plot_model(ax)
+    ax.set(xlim=(-0.005, 0.025), ylim=(-0.005, 0.06))
+    ax.fill_between((0.002, 0.015), -1, 1, alpha=0.30, color='black')
+    plt.show()
+
+    # plot a slice
+    pid_min = 0.01
+    pid_max = 0.015
+    _, ax = plt.subplots()
+    model.plot_slice(ax, pid_min, pid_max, 0.002)
+    plt.show()
 
 if __name__ == '__main__':
     main()
