25f56d042fa790edc0b502045f41549dc5c1b649
John Vouvakis Manousakis <ioannis_vm@berkeley.edu>
2024-01-11 08:40:49 -0800

    Implement fitting with separate/bundled directions
    - Update get_all_cases to handle separate and bundled data gathering approaches
    - Refactor obtain_parameters to support different model fitting methods and data approaches
    - Introduce model_classes dictionary to map methods to model classes
    - Modify dataframe multiindex construction based on data gathering approach
    - Adjust main function to loop over combinations of model fitting methods and data approaches
    - Update generate_figure function to support plotting for separate/bundled directions
    - Refactor main in plot_fit.py to handle various combinations of models and data approaches
    - Update the figure saving path to include data gathering approach in the directory structure

--- src/plot_fit.py
+++ src/plot_fit.py
@@ -12,7 +12,7 @@ import matplotlib.pyplot as plt
 from src.util import store_info
 
 
-def generate_figure(system, stories, rc, model_df, method, models_path):
+def generate_figure(system, stories, rc, model_df, method, data_gathering_approach):
     """
     Generate a figure showing the PID-RID pairs, their rolling
     quantiles and the fitted model's quantiles.  The figure is laid
@@ -27,11 +27,15 @@ def generate_figure(system, stories, rc, model_df, method, models_path):
 
     num_column_pairs = int(num_stories / 3)
 
-    num_columns = 2 * num_column_pairs
+    if data_gathering_approach == 'separate_directions':
+        num_columns = 2 * num_column_pairs
+    else:
+        num_columns = num_column_pairs
     num_rows = 3  # always the case
     box_w, box_h = 1.5, 1.5  # inch
 
-    drc = {'1': 'x', '2': 'y'}
+    if data_gathering_approach == 'separate_directions':
+        drc = {'1': 'x', '2': 'y'}
 
     plt.close()
     fig, axs = plt.subplots(
@@ -41,12 +45,16 @@ def generate_figure(system, stories, rc, model_df, method, models_path):
         sharex=True,
         sharey=True,
     )
+    axs = axs.reshape(3, -1)
     for i_col in range(num_columns):
         for i_row in range(num_rows):
             group = int(i_col / 2)
             story = str(i_row + 1 + 3 * group)
-            direction = str(i_col % 2 + 1)
-            model = df[(story, direction)]
+            if data_gathering_approach == 'separate_directions':
+                direction = str(i_col % 2 + 1)
+                model = df[(story, direction)]
+            else:
+                model = df[story]
             model.plot_model(axs[i_row, i_col])
             text = '\n'.join(
                 [
@@ -64,7 +72,10 @@ def generate_figure(system, stories, rc, model_df, method, models_path):
                 transform=axs[i_row, i_col].transAxes,
                 fontsize=4,
             )
-            text = f'{story}{drc[direction]}'
+            if data_gathering_approach == 'separate_directions':
+                text = f'{story}{drc[direction]}'
+            else:
+                text = f'{story}'
             axs[i_row, i_col].text(
                 0.05,
                 0.95,
@@ -75,16 +86,16 @@ def generate_figure(system, stories, rc, model_df, method, models_path):
                 bbox={'facecolor': 'white', 'alpha': 0.5},
                 fontsize=6,
             )
-            # axs[i_row, i_col].fill_between(
-            #     (0.002, 0.015), -1, 1, alpha=0.10, color='black'
-            # )
             axs[i_row, i_col].set(xlim=(-0.005, 0.025), ylim=(-0.005, 0.05))
     fig.suptitle(f'{stories.upper()}-STORY RC {rc.upper()} {system.upper()}')
     fig.text(0.5, 0.01, 'Residual Inter-story Drift (RID)', ha='center')
     fig.text(
         0.01, 0.50, 'Peak Inter-story Drift (PID)', va='center', rotation='vertical'
     )
-    left_adj = {'3': 0.17, '6': 0.10, '9': 0.07}
+    if data_gathering_approach == 'separate_directions':
+        left_adj = {'3': 0.17, '6': 0.10, '9': 0.07}
+    else:
+        left_adj = {'3': 0.17 * 2.0, '6': 0.10 * 2.0, '9': 0.07 * 2.0}
     fig.subplots_adjust(
         wspace=0.00,
         hspace=0.00,
@@ -94,9 +105,13 @@ def generate_figure(system, stories, rc, model_df, method, models_path):
         bottom=0.08,
     )
     # plt.show()
+    models_path = (
+        f'results/parameters/{data_gathering_approach}/{method}/models.pickle'
+    )
     plt.savefig(
         store_info(
-            f'results/figures/{method}/fit_{system}_{stories}_{rc}.pdf',
+            f'results/figures/{data_gathering_approach}'
+            f'/{method}/fit_{system}_{stories}_{rc}.pdf',
             [models_path],
         )
     )
@@ -105,48 +120,53 @@ def generate_figure(system, stories, rc, model_df, method, models_path):
 
 def main(parallel=True):
     methods = ('weibull_bilinear', 'gamma_bilinear', 'beta_bilinear')
+    data_gathering_approaches = ('separate_directions', 'bundled_directions')
 
     model_dfs = {}
-    for method in methods:
-        models_path = f'results/parameters/{method}/models.pickle'
+    for method, data_gathering_approach in product(
+        methods, data_gathering_approaches
+    ):
+        models_path = (
+            f'results/parameters/{data_gathering_approach}/'
+            f'{method}/models.pickle'
+        )
         with open(models_path, 'rb') as f:
             models = pickle.load(f)
 
         # turn the dict into a dataframe for more convenient indexing
         # operations
         model_df = pd.Series(models.values(), index=models.keys())
-        model_df.index.names = ('system', 'stories', 'rc', 'loc', 'dir')
-        model_dfs[method] = model_df
+        if data_gathering_approach == 'separate_directions':
+            model_df.index.names = ('system', 'stories', 'rc', 'loc', 'dir')
+        else:
+            model_df.index.names = ('system', 'stories', 'rc', 'loc')
+        model_dfs[(method, data_gathering_approach)] = model_df
 
+    args_list = []
+    for system, stories, rc, method, data_gathering_approach in list(
+        product(
+            ('smrf', 'scbf', 'brbf'),
+            ('3', '6', '9'),
+            ('ii', 'iv'),
+            methods,
+            data_gathering_approaches,
+        )
+    ):
+        args_list.append(
+            (
+                system,
+                stories,
+                rc,
+                model_dfs[method, data_gathering_approach],
+                method,
+                data_gathering_approach,
+            )
+        )
     if parallel is False:
         # in series
-        for system, stories, rc, method in tqdm.tqdm(
-            list(
-                product(
-                    ('smrf', 'scbf', 'brbf'),
-                    ('3', '6', '9'),
-                    ('ii', 'iv'),
-                    methods,
-                )
-            )
-        ):
-            generate_figure(
-                system, stories, rc, model_dfs[method], method, models_path
-            )
+        for args in tqdm.tqdm(args_list):
+            generate_figure(*args)
     else:
-        args_list = []
-        for system, stories, rc, method in list(
-            product(
-                ('smrf', 'scbf', 'brbf'),
-                ('3', '6', '9'),
-                ('ii', 'iv'),
-                methods,
-            )
-        ):
-            args_list.append(
-                (system, stories, rc, model_dfs[method], method, models_path)
-            )
-
         with concurrent.futures.ProcessPoolExecutor() as executor:
             futures = {
                 executor.submit(generate_figure, *args): args for args in args_list
